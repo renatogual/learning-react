885ea00068c8236ce8358605087df556513fde2b
eox-tecnologia <eox-tecnologia>
2021-01-01 22:17:29 -0300

    Utilizando Fragmentos React

---
+++ fundamentos/src/components/basicos/Fragmento.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Fragmento() {
+    return (
+        <React.Fragment>
+            <h2>Fragmento</h2>
+            <p>Utilizando fragmentos React</p>
+        </React.Fragment>
+    )
+}
--- fundamentos/src/index.js
+++ fundamentos/src/index.js
@@ -3,6 +3,7 @@ import React from 'react'
 import './index.css'
 import Primeiro from './components/basicos/Primeiro'
 import ComParametro from './components/basicos/ComParametro'
+import Fragmento from './components/basicos/Fragmento'
 
 ReactDOM.render(
     <div>
@@ -12,6 +13,7 @@ ReactDOM.render(
             aluno="José Renato"
             nota={9.0}
         />
+        <Fragmento />
     </div>, 
     document.getElementById('root')
 )
