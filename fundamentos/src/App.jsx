c09cb4c9e584d77db357a6d5544de2a4e78fbb45
eox-tecnologia <eox-tecnologia>
2021-01-02 19:23:20 -0300

    Criando componente card e aplicando estilos

--- fundamentos/src/App.jsx
+++ fundamentos/src/App.jsx
@@ -1,23 +1,39 @@
 /* eslint-disable import/no-anonymous-default-export */
 import React from "react";
+import "./App.css"
 
 import Primeiro from "./components/basicos/Primeiro";
 import ComParametro from "./components/basicos/ComParametro";
 import Fragmento from "./components/basicos/Fragmento";
 import Aleatorio from "./components/basicos/Aleatorio";
+import Card from "./components/layout/Card";
 
-export default function (props) {
+export default function App(props) {
   return (
-    <div id="app">
+    <div className="app">
       <h1>Fundamentos React</h1>
-      <Primeiro />
-      <ComParametro
-        titulo="Segundo Componente"
-        aluno="José Renato"
-        nota={9.0}
-      />
-      <Fragmento />
-      <Aleatorio min={1} max={60} />
+
+      <div className="cards">
+        <Card titulo="#01 - Primeiro Componente" cor="#080">
+          <Primeiro />
+        </Card>
+
+        <Card titulo="#02 - Componente com Parametro">
+          <ComParametro
+            titulo="Segundo Componente"
+            aluno="José Renato"
+            nota={9.0}
+          />
+        </Card>
+
+        <Card titulo="#03 - Fragmento React">
+          <Fragmento />
+        </Card>
+
+        <Card titulo="#04 - Números Aleatórios">
+          <Aleatorio min={1} max={60} />
+        </Card>
+      </div>
     </div>
   );
 }
---
+++ fundamentos/src/components/layout/Card.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import "./Card.css"
+
+export default function Card(props) {
+    const estilo = {
+        backgroundColor: props.cor || '#f00'
+    }
+    
+    return (
+        <div className="card" style={estilo}>
+            <div className="titulo">{props.titulo}</div>
+            <div className="conteudo">
+                {props.children}
+            </div>
+        </div>
+    ) 
+}
