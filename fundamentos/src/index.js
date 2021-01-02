7fa0f48b4a2d58a4407e1d1cc222d78a93f84955
eox-tecnologia <eox-tecnologia>
2021-01-01 22:07:48 -0300

    Segundo componente

--- fundamentos/src/index.js
+++ fundamentos/src/index.js
@@ -2,10 +2,16 @@ import ReactDOM from 'react-dom'
 import React from 'react'
 import './index.css'
 import Primeiro from './components/basicos/Primeiro'
+import ComParametro from './components/basicos/ComParametro'
 
 ReactDOM.render(
     <div>
         <Primeiro />
+        <ComParametro 
+            titulo="Segundo Componente" 
+            aluno="José Renato"
+            nota={9.0}
+        />
     </div>, 
     document.getElementById('root')
 )
