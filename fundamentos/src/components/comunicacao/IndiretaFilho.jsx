9601fb50afd14e4b9d6444e41fea85422720c0aa
eox-tecnologia <eox-tecnologia>
2021-01-03 12:14:21 -0300

    Entendo um pouco sobre componentes com estado

--- fundamentos/src/components/comunicacao/IndiretaFilho.jsx
+++ fundamentos/src/components/comunicacao/IndiretaFilho.jsx
@@ -1,10 +1,13 @@
 import React from "react";
 
 export default function IndiretaFilho(props) {
+  const gerarIdade = () => parseInt(Math.random() * (70 - 50)) + 50;
   return (
     <div>
       <div>Filho</div>
-      <button onClick={_ => props.quandoClicar('João', 23, true)}>Fornecer Informações</button>
+      <button onClick={(_) => props.quandoClicar("João", gerarIdade(), true)}>
+        Fornecer Informações
+      </button>
     </div>
   );
 }
--- fundamentos/src/components/comunicacao/IndiretaPai.jsx
+++ fundamentos/src/components/comunicacao/IndiretaPai.jsx
@@ -1,14 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 import IndiretaFilho from "./IndiretaFilho";
 
 export default function IndiretaPai(props) {
+  let [nome, setNome] = useState('?')
+  let [idade, setIdade] = useState(0)
+  let [nerd, setNerd] = useState(false)
+
   function fornecerInformacoes(nome, idade, nerd) {
-    console.log(nome, idade, nerd);
+    setNome(nome)
+    setIdade(idade)
+    setNerd(nerd)
   }
-  
+
   return (
     <div>
-      <div>Pai</div>
+      <span>{nome} </span>
+      <span><strong>{idade} </strong></span>
+      <span>{nerd ? 'Verdadeiro' : 'Falso'}</span>
       <IndiretaFilho quandoClicar={fornecerInformacoes}></IndiretaFilho>
     </div>
   );
