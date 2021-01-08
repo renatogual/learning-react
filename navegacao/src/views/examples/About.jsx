8b44bc0a1451294825184fc00e8a9230f83b9984
eox-tecnologia <eox-tecnologia>
2021-01-08 12:51:18 -0300

    Criando componentes

---
+++ navegacao/src/views/examples/About.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const About = props => {
+    <div className="About">
+        <h1>Sobre</h1>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Deleniti aspernatur est quasi ex hic nulla libero a sunt et ea, ratione rem facere facilis quis dicta maxime id sapiente possimus!</p>
+    </div>
+}
+
+export default About
