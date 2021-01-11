e60b8ceffc6d40e36cb565dc3f2567a99532f3d1
eox-tecnologia <eox-tecnologia>
2021-01-11 13:40:20 -0300

    Criando componentes com rotas

--- navegacao/src/components / layout/Content.jsx
+++ navegacao/src/components / layout/Content.jsx
@@ -3,12 +3,16 @@ import { Route, Switch } from 'react-router-dom'
 import './Content.css'
 import Home from '../../views/examples/Home'
 import About from '../../views/examples/About'
+import Param from '../../views/examples/Param'
 
 const Content = props => (
     <main className="Content">
         <Switch>
             <Route path="/about">
                 <About />
+            </Route>
+            <Route path="/param/:id">
+                <Param />
             </Route>
             <Route path="/">
                 <Home />
--- navegacao/src/components / layout/Menu.jsx
+++ navegacao/src/components / layout/Menu.jsx
@@ -1,15 +1,23 @@
 import React from 'react'
 import './Menu.css'
 
+import { Link } from "react-router-dom";
+
 const Menu = props => (
     <aside className="Menu">
         <nav>
             <ul>
                 <li>
-                    <a href="/">Inicio</a>
+                    <Link to="/">Inicio</Link>
                 </li>
                 <li>
-                    <a href="/">Sobre</a>
+                    <Link to="/param/123">Param #01</Link>
+                </li>
+                <li>
+                    <Link to="/param/legal">Param #02</Link>
+                </li>
+                <li>
+                    <Link to="/about">Sobre</Link>
                 </li>
             </ul>
         </nav>
