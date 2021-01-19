0eb3c60deec6b37dd9f46115a1ae3bc73cba5ee3
renatogual <renatogual@hotmail.com>
2021-01-19 19:56:49 -0300

    Utilizando useContext

--- hooks/src/views/App.jsx
+++ hooks/src/views/App.jsx
@@ -5,15 +5,19 @@ import { BrowserRouter as Router } from 'react-router-dom'
 import Menu from '../components/layout/Menu'
 import Content from '../components/layout/Content'
 
+import DataContext, { data } from '../data/DataContext'
+
 const App = props => {
 
     return (
-        <div className="App">
-            <Router>
-                <Menu />
-                <Content />
-            </Router>
-        </div>
+        <DataContext.Provider value={data}>
+            <div className="App">
+                <Router>
+                    <Menu />
+                    <Content />
+                </Router>
+            </div>
+        </DataContext.Provider>
     )
 }
 
--- hooks/src/views/examples/UseContext.jsx
+++ hooks/src/views/examples/UseContext.jsx
@@ -1,13 +1,21 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import PageTitle from '../../components/layout/PageTitle'
 
+import DataContext from '../../data/DataContext'
+
 const UseContext = (props) => {
+    const context = useContext(DataContext)
+
     return (
         <div className="UseContext">
             <PageTitle
                 title="Hook UseContext"
                 subtitle="Aceita um objeto de contexto e retorna o valor atual do contexto!"
             />
+
+            <div className="center">
+                <span className="text">{context.text}</span>
+            </div>
         </div>
     )
 }
