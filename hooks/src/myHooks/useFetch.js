40714d9a5b05ef8081b698b4a27a0a63bd463bee
renatogual <renatogual@hotmail.com>
2021-01-23 18:45:31 -0300

    Exercicio 02 useCustom

---
+++ hooks/src/myHooks/useFetch.js
@@ -0,0 +1,19 @@
+import { useState } from "react"
+
+export const useFetch = (url, method = 'GET') => {
+    const [response, setResponse] = useState({
+        data: null,
+        loading: true
+    })
+
+    useEffect(() => {
+        fetch(url, { method })
+            .then(res => res.json())
+            .then(json => setResponse({
+                data: json,
+                loading: false
+            }))
+    }, [url, method])
+
+    return response
+}
