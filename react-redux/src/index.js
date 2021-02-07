bcc4f1d04a5b3b55b28ced48a888210aee2b2963
renatogual <renatogual@hotmail.com>
2021-02-07 16:17:59 -0300

    Configurando o redux

--- react-redux/src/index.js
+++ react-redux/src/index.js
@@ -3,9 +3,14 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
+import { Provider } from 'react-redux';
+import storeConfig from './store/storeConfig'
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Provider store={storeConfig()}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </Provider>,
   document.getElementById('root')
 );
