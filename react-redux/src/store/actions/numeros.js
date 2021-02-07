a2c054d8aa1c7a2fcf0b3733108b9eab46f91d29
renatogual <renatogual@hotmail.com>
2021-02-07 19:09:09 -0300

    Criando reducers

---
+++ react-redux/src/store/actions/numeros.js
@@ -0,0 +1,14 @@
+
+export function alterarNumeroMin(novoNum) {
+  return {
+    type: 'NUM_MIN_ALTERADO',
+    payload: novoNum
+  }
+}
+
+export function alterarNumeroMax(novoNum) {
+  return {
+    type: 'NUM_MAX_ALTERADO',
+    payload: novoNum
+  }
+}
--- react-redux/src/store/storeConfig.js
+++ react-redux/src/store/storeConfig.js
@@ -2,14 +2,25 @@ import { createStore, combineReducers } from 'redux'
 
 const reducers = combineReducers({
   numeros: function (state, action) {
-    console.log(state, ' ', action);
-    return {
-      min: 1,
-      max: 10
+    switch (action.type) {
+      case 'NUM_MIN_ALTERADO':
+        return {
+          ...state,
+          min: action.payload
+        }
+      case 'NUM_MAX_ALTERADO':
+        return {
+          ...state,
+          max: action.payload
+        }
+      default:
+        return {
+          min: 1,
+          max: 10
+        };
     }
   },
   nomes: function (state, action) {
-    console.log(state, ' ', action);
     return [
       'José',
       'Renato'
